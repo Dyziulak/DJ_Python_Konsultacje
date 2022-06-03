513af623b420d42b7bb2725df78756e908e41171
daniel <danieljamrozik5@gmail.com>
2022-06-03 14:17:53 +0200

    Ostatnia Edycja 03.06.2022r

--- p01.py
+++
@@ -1,9 +0,0 @@
-def main():
-    x = input("Podaj pierwszą liczbę: ")
-    x = int(x)
-    y = input("Podaj drugą liczbę: ")
-    y = int(y)
-    z = x * y
-    print("Iloczyn równa się: ", z)
-
-main()
